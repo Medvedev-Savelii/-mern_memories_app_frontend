7bea53791dff917a832143f19cabe8cd7f1c7ad8
sava <savamedvedev@gmail.com>
2022-07-30 18:50:07 +0800

    therd commit

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core/";
+
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import useStyles from "./styles";
+
+const Post = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={
+          "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+        }
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">Sava</Typography>
+        <Typography variant="body2">12 October 2021</Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small">
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary" component="h2">
+          one,two,three
+        </Typography>
+      </div>
+      <Typography
+        className={classes.title}
+        gutterBottom
+        variant="h5"
+        component="h2"
+      >
+        Lorem ipsum dolor sit, amet consectetur adipisicing.
+      </Typography>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          consequatur, unde accusantium doloremque iste distinctio autem
+          incidunt commodi magnam cupiditate, eius rerum mollitia tempore
+          consectetur quasi alias tempora eos optio?
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          <ThumbUpAltIcon fontSize="small" /> Like 4{" "}
+        </Button>
+        <Button size="small" color="primary">
+          <DeleteIcon fontSize="small" /> Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+export default Post;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      <Grid item xs={12} sm={6} md={6}>
+        <Post />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Posts;
