0f35279a4b29f87e7e65e7efcbd620c3d62be7c7
sava <savamedvedev@gmail.com>
2022-08-06 19:20:40 +0800

    eight-commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-// import Auth from "./components/Auth/Auth";
+import Auth from "./components/Auth/Auth";
 
 const App = () => (
   <BrowserRouter>
     <Container maxWidth="lg">
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
-        {/* <Route path="/auth" exact component={Auth} /> */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/auth" component={Auth} />
       </Switch>
     </Container>
   </BrowserRouter>
